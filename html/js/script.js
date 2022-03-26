27240b88e64c32dc330a8ec7c808beb99cea9116
aaritmendi <aaritmendi7@gmail.com>
2022-03-26 13:23:54 +0100

    general update

---
+++ html/js/script.js
@@ -0,0 +1,29 @@
+var ano = (new Date).getFullYear();
+$(document).ready( function() {	
+$(".algo").typed({
+        strings: ["Adrián Aritmendi", "Web Developer", "Geek", "Técnico"],
+        typeSpeed: 12,
+        loop:true,
+        backDelay: 2400
+})
+$("#fecha").text( ano );
+;
+$(window).scroll(function(){
+    if(this.scrollY > 20){
+      $(".navbar").addClass("sticky");
+      $(".goTop").fadeIn();
+    }
+    else{
+      $(".navbar").removeClass("sticky");
+      $(".goTop").fadeOut();
+    }
+  });
+
+  $(".goTop").click(function(){scroll(0,0)});
+
+  $('.menu-toggler').click(function(){
+    $(this).toggleClass("active");
+    $(".navbar-menu").toggleClass("active");
+  });
+});
+
